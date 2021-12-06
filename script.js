7a89b8e0c5de3fab05504256dfcd447643d4be24
Ardonplay <ardonplay@gmail.com>
2021-12-06 21:11:46 +0300

    upd

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ const ham = document.getElementById('ham');
 ham.addEventListener('click', function () {
   document.body.classList.toggle('nav-is-toggled');
 });
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
